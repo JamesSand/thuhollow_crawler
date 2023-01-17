fe797787d4ac51c6c98d5dd173aa82bef1a3442d
JamesSand <shazz20@mails.tsinghua.edu.cn>
2023-01-17 12:50:13 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## 树洞爬虫
+
+https://thuhollow.github.io/
+
+替换自己的 user agent 和 user token
+
+写的比较烂。。。轻喷
+
+有需求可以在 issue 里边题
---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+
+headers = {
+    "user-agent": "",
+    "user-token": ""
+}
+
+x = requests.get("https://api.shudongdehouduan.click//_api/v1/getlist?p=1&order_mode=2&room_id=", headers=headers)
+
+response = json.loads(x.text)
+data = response["data"]
+
+target_id = 1
+
+for i, item in enumerate(data):
+    if i != target_id: continue
+
+    pid = item["pid"]
+
+    y = requests.get(f"https://api.shudongdehouduan.click//_api/v1/getcomment?pid={pid}", headers=headers)
+    response = json.loads(y.text)
+
+    comment = response["data"]
+
+    with open(f"record{i}_pid{pid}.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(comment, indent=4, ensure_ascii=False))
+
+    with open(f"record{i}_pid{pid}.txt", "w", encoding="utf-8") as f:
+        for sentence in comment:
+            f.write(sentence["text"] + "\n\n")
+
+
+    
+    break
+
+
+
+
