00c227ae4244955a399090865f09da49c1d37cb1
JamesSand <shazz20@mails.tsinghua.edu.cn>
2023-07-14 08:06:11 +0800

    add user agent, and token

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import json
 
 
 headers = {
-    "user-agent": "",
-    "user-token": ""
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.79",
+    "user-token": "Hf3uOkLbkBjQkOnu"
 }
 
-x = requests.get("https://api.shudongdehouduan.click//_api/v1/getlist?p=1&order_mode=2&room_id=", headers=headers)
+# 获取热榜 id
+x = requests.get("https://api.shudongdehouduan.click//_api/v1/getlist?p=1&order_mode=2&room_id=", 
+                 headers=headers)
 
 response = json.loads(x.text)
 data = response["data"]
 
-target_id = 1
+# 爬取热榜第一 
+# target_id = 1
 
 for i, item in enumerate(data):
-    if i != target_id: continue
+    # if i != target_id: continue
 
     pid = item["pid"]
     head = item["text"]
